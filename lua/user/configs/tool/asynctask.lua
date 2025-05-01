7b3725f45ec6653c7dc8647437f58424b84314e3
jczhang02 <jczhang@live.it>
2025-05-02 05:56:33 +0800

    asynctask/asyncrun: options setting ".pro" as rootmarks

--- lua/user/configs/tool/asynctask.lua
+++ lua/user/configs/tool/asynctask.lua
@@ -1,5 +1,5 @@
 return function()
 	vim.g.asynctasks_filetype = "ini"
 	vim.g.asynctask_template = "~/.config/nvim/.task.ini"
-	vim.g.asynctask_rootmarks = { ".git", "pyproject.toml" }
+	vim.g.asynctask_rootmarks = { ".git", "pyproject.toml", ".pro" }
 end
